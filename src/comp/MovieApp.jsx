873a6f8047d887c82e0f64d86f61c0c399bc3898
Your Name <you@example.com>
2023-10-23 00:13:11 -0700

    this is the new code"

--- src/comp/MovieApp.jsx
+++ src/comp/MovieApp.jsx
@@ -3,21 +3,36 @@ import nikeShoesImage from "../img/nike-shoes.jpg";
 
 const MovieApp = (props) => {
   const [movies, setMovies] = useState([]);
-  const [value, setValue] = useState("");
-  const [userMovie, setUserMovie] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [userMovies, setUserMovies] = useState([]);
 
-  const getMovies = async () => {
+  const getMovies = async (searchTerm) => {
     const apiKey = "b23286c3a57cc65c9dbb4161cbf89f13";
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US`;
+    let url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US`;
+
+    if (searchTerm) {
+      url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${searchTerm}`;
+    }
+
     const res = await fetch(url);
     const data = await res.json();
-    setMovies(data.results);
+
+    if (searchTerm) {
+      setUserMovies(data.results);
+    } else {
+      setMovies(data.results);
+    }
   };
 
   useEffect(() => {
     getMovies();
-  }, [getMovies]);
-  
+  }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    getMovies(searchValue);
+  };
+
   return (
     <div>
       <section className="container">
@@ -25,16 +40,16 @@ const MovieApp = (props) => {
         <section className="search">
           <div className="container">
             <div id="alert"></div>
-            <form className="search-form" onSubmit={(e)=> e.preventdefault ()}>
+            <form className="search-form" onSubmit={handleSearch}>
               <div className="search-flex">
                 <input
                   type="text"
                   name="search-term"
                   id="search-term"
                   placeholder="Enter search term"
-                  value={value}
+                  value={searchValue}
                   onChange={(e) => {
-                    setValue(e.target.value);
+                    setSearchValue(e.target.value);
                   }}
                 />
                 <button className="btn" type="submit">
@@ -45,27 +60,45 @@ const MovieApp = (props) => {
           </div>
         </section>
         <div id="popular-movies" className="grid">
-          {movies.map((movie) => {
-            return (
-              <div className="card" key={movie.id}>
-                <a href="#">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                    alt={movie.title}
-                    className="card-img-top"
-                  />
-                </a>
-                <div className="card-body">
-                  <h5 className="card-title">{movie.title}</h5>
-                  <p className="card-text">
-                    <small className="text-muted">
-                      Release Date: {movie.release_date}
-                    </small>
-                  </p>
+          {searchValue
+            ? userMovies.map((movie) => (
+                <div className="card" key={movie.id}>
+                  <a href="#">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                      alt={movie.title}
+                      className="card-img-top"
+                    />
+                  </a>
+                  <div className="card-body">
+                    <h5 className="card-title">{movie.title}</h5>
+                    <p className="card-text">
+                      <small className="text-muted">
+                        Release Date: {movie.release_date}
+                      </small>
+                    </p>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              ))
+            : movies.map((movie) => (
+                <div className="card" key={movie.id}>
+                  <a href="#">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                      alt={movie.title}
+                      className="card-img-top"
+                    />
+                  </a>
+                  <div className="card-body">
+                    <h5 className="card-title">{movie.title}</h5>
+                    <p className="card-text">
+                      <small className="text-muted">
+                        Release Date: {movie.release_date}
+                      </small>
+                    </p>
+                  </div>
+                </div>
+              ))}
         </div>
       </section>
     </div>
